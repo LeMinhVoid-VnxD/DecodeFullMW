5c3010431fe27f187babd5c50245000f9f7e709e
LeMinhVoid-VnxD <dop426719@gmail.com>
2026-08-18 10:44:51 +0700

    Part 3: 6900+ Lua scripts, 1100+ CSV tables and language assets

---
+++ 01_Nguon_MiniWorld_Data410/02_Lua_Scripts_va_Bang_CSV_script_res/commonresource/script/ui/accelkeysf12_init.lua
@@ -0,0 +1,9 @@
+
+-------------------------------------
+--ui/mobile/account.xml
+--ui/mobile/account.lua
+--ui/mobile/mvc/developer/DeveloperInterface.lua
+--ui/mobile/mvc/virtualAvatar/virtualAvatarCenter/VirtualAvatarCenter.xml
+
+--ui/mobile/tips.xml
+miniui/module/adventure/ToolBoxFrame/ToolBoxFrameAutoGen.lua
